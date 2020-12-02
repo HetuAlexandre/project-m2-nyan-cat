f1a1db7c362f91a77f1f51251b14a2d41613dc1f
Alexandre <alex_18h@hotmail.com>
2020-12-01 23:07:02 -0500

    project game

--- js/Engine.js
+++ js/Engine.js
@@ -1,73 +1,93 @@
-// The engine class will only be instantiated once. It contains all the logic
-// of the game relating to the interactions between the player and the
-// enemy and also relating to how our enemies are created and evolve over time
 class Engine {
-  // The constructor has one parameter. It will refer to the DOM node that we will be adding everything to.
-  // You need to provide the DOM node when you create an instance of the class
   constructor(theRoot) {
-    // We need the DOM element every time we create a new enemy so we
-    // store a reference to it in a property of the instance.
     this.root = theRoot;
-    // We create our hamburger.
-    // Please refer to Player.js for more information about what happens when you create a player
     this.player = new Player(this.root);
-    // Initially, we have no enemies in the game. The enemies property refers to an array
-    // that contains instances of the Enemy class
     this.enemies = [];
-    // We add the background image to the game
+    this.bonuses = [];
+    this.score = new Text(this.root, 740, 10, 25);
+    this.timelapsed = 0;
     addBackground(this.root);
   }
 
-  // The gameLoop will run every few milliseconds. It does several things
-  //  - Updates the enemy positions
-  //  - Detects a collision between the player and any enemy
-  //  - Removes enemies that are too low from the enemies array
   gameLoop = () => {
-    // This code is to see how much time, in milliseconds, has elapsed since the last
-    // time this method was called.
-    // (new Date).getTime() evaluates to the number of milliseconds since January 1st, 1970 at midnight.
     if (this.lastFrame === undefined) {
       this.lastFrame = new Date().getTime();
     }
 
     let timeDiff = new Date().getTime() - this.lastFrame;
-
     this.lastFrame = new Date().getTime();
-    // We use the number of milliseconds since the last call to gameLoop to update the enemy positions.
-    // Furthermore, if any enemy is below the bottom of our game, its destroyed property will be set. (See Enemy.js)
+    this.timelapsed += timeDiff;
+    this.score.update(this.timelapsed);
     this.enemies.forEach((enemy) => {
       enemy.update(timeDiff);
     });
 
-    // We remove all the destroyed enemies from the array referred to by \`this.enemies\`.
-    // We use filter to accomplish this.
-    // Remember: this.enemies only contains instances of the Enemy class.
+    this.bonuses.forEach((bonus) => {
+      bonus.update(timeDiff);
+    });
     this.enemies = this.enemies.filter((enemy) => {
       return !enemy.destroyed;
     });
+    this.bonuses = this.bonuses.filter((bonus) => {
+      return !bonus.destroyed;
+    });
 
-    // We need to perform the addition of enemies until we have enough enemies.
     while (this.enemies.length < MAX_ENEMIES) {
-      // We find the next available spot and, using this spot, we create an enemy.
-      // We add this enemy to the enemies array
       const spot = nextEnemySpot(this.enemies);
       this.enemies.push(new Enemy(this.root, spot));
     }
 
-    // We check if the player is dead. If he is, we alert the user
-    // and return from the method (Why is the return statement important?)
+    while (this.bonuses.length < MAX_BONUSES) {
+      const spot = nextBonusSpot(this.bonuses);
+      this.bonuses.push(new Bonus(this.root, spot));
+    }
+
+    if (this.isPlayerBonus()) {
+      this.timelapsed = this.timelapsed + 700;
+    }
+
     if (this.isPlayerDead()) {
-      window.alert('Game over');
+      let text = new Text(this.root, 50, 300, 45);
+        text.update("You failed to escape the Empire ...");
+      let reset = document.createElement("button");
+        reset.className = "start";
+        reset.innerText = "RETRY";
+        app.appendChild(reset);
+        reset.addEventListener("click", () => this.resetGame(reset, text));
       return;
     }
 
-    // If the player is not dead, then we put a setTimeout to run the gameLoop in 20 milliseconds
     setTimeout(this.gameLoop, 20);
   };
 
-  // This method is not implemented correctly, which is why
-  // the burger never dies. In your exercises you will fix this method.
-  isPlayerDead = () => {
-    return false;
+  resetGame(reset, text) {
+    reset.style.display = "none";
+    this.gameLoop();
+    text.update("");
+    this.timelapsed = 0;
+    this.lastFrame = new Date().getTime();
+    this.score.update(0);
+  }
+
+  isPlayerBonus = () => {
+    let isPlayerBonus = false;
+
+    this.bonuses.forEach((bonus) => {
+      if (this.player.x === bonus.x && this.player.y < bonus.y + BONUS_HEIGHT) {
+        isPlayerBonus = true;
+      }
+    });
+    return isPlayerBonus;
   };
+
+  isPlayerDead = () => {
+    let isPlayerDead = false;
+      this.enemies.forEach((enemy) => {
+        if (this.player.x === enemy.x && this.player.y < enemy.y + ENEMY_HEIGHT) {
+          isPlayerDead = true;
+      }
+    });
+      return isPlayerDead;
+  }; 
 }
+
